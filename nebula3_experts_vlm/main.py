e7f64539801249015f673c7ad5ddd51f81152910
AnaRhisT94 <bebetteryou2015@gmail.com>
2022-05-18 12:30:54 +0000

    refactored experts_vlm

--- nebula3_experts_vlm/main.py
+++ nebula3_experts_vlm/main.py
@@ -1,63 +1,30 @@
 # from typing import Optional
 from fastapi import FastAPI
 
-# tags_metadata = [
-#     {
-#         "name": "status",
-#         "description": "View running status of pipeline steps.",
-#     },
-#     {
-#         "name": "set",
-#         "description": "Set a configuration: <cfg_name>=<value> where cfg_name is one of the configurations \
-#         run 'cfg' command to see possible configurations.",
-#     },
-#     {
-#         "name": "cfg",
-#         "description": "list all editable configurations.",
-#     },
-# ]
-
-# app = FastAPI(openapi_tags=tags_metadata)
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.get("/status")
-# def get_status(q: Optional[str] = None):
-#     return {"status": "running" }
-
-# @app.post("/set}", tags=['set'] )
-# def post_status(q: Optional[str] = None):
-#     return {"set": 'ok', "q": q }
-
-# @app.get("/cfg}", tags=['cfg'] )
-# def get_config(q: Optional[str] = None):
-#     return {"set": 'ok', "q": q }
-
 from typing import Optional
 
-from experts.service.base_expert import BaseExpert
-from experts.app import ExpertApp
-from experts.common.defines import *
+from nebula3_experts.experts.service.base_expert import BaseExpert
+from nebula3_experts.experts.app import ExpertApp
+from nebula3_experts.experts.common.defines import *
 
 import sys
 sys.path.insert(0, 'nebula3_vlm/')
+sys.path.insert(0, 'nebula3_experts/')
+sys.path.insert(0, 'nebula3_experts/experts/')
 from nebula3_vlm.models.clip_ms.clip_ms import Model
 
-class MyExpert(BaseExpert):
+class VlmExpert(BaseExpert):
     def __init__(self):
         super().__init__()
         # after init all
         self.set_active()
 
     def get_name(self):
-        return "MyExpert"
+        return "VlmExpert"
     def get_cfg(self) -> str:
         return {}
     def get_dependency(self) -> str:
-        return "MyExpert"
+        return "VlmExpert"
 
 
     def get_cfg(self):
@@ -110,7 +77,7 @@ class MyExpert(BaseExpert):
             result = clip_model.encode_video(data)
             return { 'result': result}
 
-my_expert = MyExpert()
+my_expert = VlmExpert()
 expert_app = ExpertApp(expert=my_expert)
 app = expert_app.get_app()
 expert_app.run()
